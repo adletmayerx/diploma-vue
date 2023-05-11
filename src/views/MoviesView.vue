6f73e006d3d07c7c46db03523247d79371c5e29f
adletmayerx <adletmayerx@users.noreply.github.com>
2023-05-11 17:34:05 +0500

    feat: add MoviesView

---
+++ src/views/MoviesView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Promo, About, Technologies, Student, Footer } from "../components";
+</script>
+
+<template>
+  <Promo />
+  <About />
+  <Technologies />
+  <Student />
+  <Footer />
+</template>
