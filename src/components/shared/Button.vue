eda2a9111823c5d582d0ca36268180d749f5cf06
adletmayerx <adletmayerx@users.noreply.github.com>
2023-05-11 16:09:27 +0500

    feat: add Button component

---
+++ src/components/shared/Button.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import clsx from "clsx";
+import { Component } from "vue";
+
+type Props = {
+  children: string | Component;
+  className?: string;
+  type?: "button" | "submit" | "reset";
+  title?: string;
+  disabled?: boolean;
+  onClick?: (payload: MouseEvent) => void | undefined;
+};
+
+withDefaults(defineProps<Props>(), {
+  type: "button",
+  disabled: false,
+});
+</script>
+
+<template>
+  <button
+    :type="type"
+    :class="clsx('cursor-pointer transition-opacity duration-300 hover:opacity-80', className && className)"
+    :title="title"
+    :disabled="disabled"
+    :onClick="onClick"
+  >
+    {children}
+  </button>
+</template>
