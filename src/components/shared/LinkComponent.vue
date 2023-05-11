f6d4c1d8d6c9178cd9cd5f0967d027e2d7d16ee7
adletmayerx <adletmayerx@users.noreply.github.com>
2023-05-11 17:18:30 +0500

    feat: add LinkComponent

---
+++ src/components/shared/LinkComponent.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import clsx from "clsx";
+
+type Props = {
+  link: string;
+  className?: string;
+};
+
+defineProps<Props>();
+</script>
+
+<template>
+  <a
+    :href="link"
+    target="_blank"
+    :class="clsx('transition-opacity duration-300 hover:opacity-70', className && className)"
+  >
+    <slot></slot>
+  </a>
+</template>
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,5 +1,6 @@
 import Button from "./Button.vue";
 import SectionHeading from "./SectionHeading.vue";
 import SectionText from "./SectionText.vue";
+import LinkComponent from "./LinkComponent.vue";
 
-export { Button, SectionHeading, SectionText };
+export { Button, SectionHeading, SectionText, LinkComponent };
