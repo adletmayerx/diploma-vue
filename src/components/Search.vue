5497a7b6bc80e58e80d385f9b1899311a755af46
adletmayerx <asadrtdinov2003@gmail.com>
2023-05-12 03:44:14 +0500

    feat: add Search

---
+++ src/components/Search.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Button, Switch } from "./shared";
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap-11 px-4 pt-20 pb-4 md:px-7 xl:px-16">
+    <div class="flex gap-2 md:gap-7 w-full">
+      <input
+        type="text"
+        class="h-11 grow rounded bg-dark-charcoal p-4 text-xs text-suva-grey placeholder:text-suva-grey"
+        placeholder="Введите название фильма"
+      />
+      <Button class="h-11 w-16 rounded-full bg-royal-blue text-xs text-gray-50"> Найти </Button>
+    </div>
+    <div class="flex items-center justify-center gap-3">
+      <Switch />
+      <p class="text-xs text-gray-50">Короткометражки</p>
+    </div>
+    <div class="h-px w-full bg-charcoal"></div>
+  </div>
+</template>
