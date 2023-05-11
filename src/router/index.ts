f2d410f4f7cdb09d4d23517bc50248d056c852a3
adletmayerx <adletmayerx@users.noreply.github.com>
2023-05-11 17:33:51 +0500

    refactor: views export/import

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { HomeView, MoviesView } from "../views";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +8,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/movies",
+      name: "movies",
+      component: MoviesView,
     },
   ],
 });
---
+++ src/views/index.ts
@@ -0,0 +1,4 @@
+import HomeView from "./HomeView.vue";
+import MoviesView from "./MoviesView.vue";
+
+export { HomeView, MoviesView };
