755d2f00a817f857905857d7b383dc333da40841
adletmayerx <asadrtdinov2003@gmail.com>
2023-05-12 03:44:56 +0500

    feat: update MoviesView

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,9 @@ import Student from "./Student.vue";
 import Footer from "./Footer.vue";
 import Header from "./Header.vue";
 import Layout from "./Layout.vue";
+import Search from "./Search.vue";
+import Movies from "./Movies.vue";
+import MoviesCard from "./MoviesCard.vue";
 
 
-export { Promo, About, Technologies, Student, Footer, Header, Layout };
+export { Promo, About, Technologies, Student, Footer, Header, Layout, Search, Movies, MoviesCard };
--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,11 +1,57 @@
 <script setup lang="ts">
-import { Promo, About, Technologies, Student, Footer } from "../components";
+import { Search, Movies, MoviesCard } from "../components";
+import { Button } from "../components/shared";
+
+const MOVIES = [
+  {
+    image: "/movies-image.png",
+    description: "movie desc",
+    title: "33 слова о дизайне",
+    duration: "1ч 47м",
+    isSaved: true,
+  },
+  {
+    image: "/movies-image.png",
+    description: "movie desc",
+    title: "33 слова о дизайне",
+    duration: "1ч 47м",
+    isSaved: false,
+  },
+  {
+    image: "/movies-image.png",
+    description: "movie desc",
+    title: "33 слова о дизайне",
+    duration: "1ч 47м",
+    isSaved: true,
+  },
+  {
+    image: "/movies-image.png",
+    description: "movie desc",
+    title: "33 слова о дизайне",
+    duration: "1ч 47м",
+    isSaved: true,
+  },
+  {
+    image: "/movies-image.png",
+    description: "movie desc",
+    title: "33 слова о дизайне",
+    duration: "1ч 47м",
+    isSaved: false,
+  },
+];
 </script>
 
 <template>
-  <Promo />
-  <About />
-  <Technologies />
-  <Student />
-  <Footer />
+  <div class="flex flex-col items-center bg-mine-shaft">
+    <div class="flex w-full max-w-7xl flex-col">
+      <Search />
+      <Movies :movies='MOVIES' :CardComponent='MoviesCard' cardType="movies" />
+      <Button
+        class="mx-auto mt-12 mb-20 flex h-9 w-60 items-center justify-center rounded-md bg-dark-charcoal text-xs font-medium text-gray-50 md:w-80"
+      >
+        Ещё
+      </Button>
+      <Footer />
+    </div>
+  </div>
 </template>
