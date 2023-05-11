00481907f05638a3fc17185b8ae26f273c6c7d40
adletmayerx <adletmayerx@users.noreply.github.com>
2023-05-11 16:50:23 +0500

    fix: tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
