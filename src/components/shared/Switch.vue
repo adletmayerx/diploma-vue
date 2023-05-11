fed127d3adddffd861d49cf2211b1ad5b7203266
adletmayerx <asadrtdinov2003@gmail.com>
2023-05-12 03:44:07 +0500

    feat: add Switch component

---
+++ src/components/shared/Switch.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex h-5 w-9 cursor-pointer justify-start rounded-3xl bg-night-rider p-0.5">
+    <span class="h-4 w-4 rounded-full border border-gray-50 bg-shamrock"></span>
+  </div>
+</template>
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,5 +2,6 @@ import Button from "./Button.vue";
 import SectionHeading from "./SectionHeading.vue";
 import SectionText from "./SectionText.vue";
 import LinkComponent from "./LinkComponent.vue";
+import Switch from "./Switch.vue";
 
-export { Button, SectionHeading, SectionText, LinkComponent };
+export { Button, SectionHeading, SectionText, LinkComponent, Switch };
