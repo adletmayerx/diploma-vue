b5f71de770b10a8fbd1b84888d50753c17911679
adletmayerx <asadrtdinov2003@gmail.com>
2023-05-12 04:12:41 +0500

    feat: update Header links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,25 @@
 <script setup lang="ts">
 import clsx from "clsx";
 import { HeaderLogoIcon } from "./icons";
-import { RouterLink, useRoute } from "vue-router";
+import { RouterLink, useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
+const router = useRouter();
 </script>
 
 <template>
   <header :class="clsx('flex justify-center', [route.path === '/' ? 'bg-tiber' : 'bg-mine-shaft'])">
     <div class="flex w-full max-w-7xl items-center justify-between px-4 py-5 md:px-8 xl:px-16">
-      <HeaderLogoIcon />
+      <HeaderLogoIcon @click="router.push('/')" class="cursor-pointer" />
       <div class="flex justify-start gap-4 md:gap-8">
-        <RouterLink class="inline-flex items-center justify-center text-xs font-medium text-gray-50" to="/register">
-          Регистрация
+        <RouterLink class="inline-flex items-center justify-center text-xs font-medium text-gray-50" to="/movies">
+          Фильмы
         </RouterLink>
         <RouterLink
           class="inline-flex h-7 w-14 items-center justify-center rounded bg-shamrock align-middle text-xs font-medium leading-relaxed text-gray-900 md:h-8 md:w-20"
-          to="/login"
+          to="/favorites"
         >
-          Войти
+          Избранное
         </RouterLink>
       </div>
     </div>
